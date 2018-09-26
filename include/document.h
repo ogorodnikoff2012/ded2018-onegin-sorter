475f09a54413066a2d84a8344ab6a9a808a2098d
Vladimir Ogorodnikov <volodya5000@gmail.com>
2018-09-27 01:19:21 +0300

    Documentation

--- include/document.h
+++ include/document.h
@@ -3,18 +3,35 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/** @file */
+
 typedef char * line_t;
 
+/** Text file separated into lines by `'\\n'` symbol */
 typedef struct {
-    char* data;
-    int data_size;
-    line_t* lines;
-    int lines_cnt;
+    char* data; ///< Raw data from file
+    int data_size; ///< Raw data size; used for internal purposes
+    line_t* lines; ///< Array of lines of text
+    int lines_cnt; ///< Length of `lines` array
 } document_t;
 
+/** Open file `filename` and read it
+ * \return pointer to the document if no errors occured or NULL otherwise */
 document_t* read_document(const char* filename);
+
+/** Check if the document is correct utf-8 file
+ * \param [out] err_pos If file is malformed, position of the first illegal byte is written here
+ * */
 bool check_document(const document_t* document, int* err_pos);
+
+/** Write lines of the document to the file `filename`
+ * \return `true` if succeded and `false` otherwise
+ * */
 bool print_document(const document_t* document, const char* filename);
+
+/** Free al structures related to the document
+ * */
 void close_document(document_t* document);
 
+/** Get symbol by its position in original file */
 int32_t symbol_at(const document_t* document, int pos);
--- include/unicode.h
+++ include/unicode.h
@@ -1,7 +1,22 @@
 #pragma once
 
+/** @file */
+
 #include "unicode_tables.h"
 
+/** Read next utf8-encoded symbol from char buffer and move the pointer
+ * \param [in, out] pos Is changed if there is a valid encoded symbol
+ * \return code of symbol or -1 if it is invalid */
 int32_t next_symbol(const char** pos);
+
+/** Compare two utf8-encoded strings ignoring all symbols except letters and case-insensitively
+ *  and return negative, zero or positive value if first string is less, equal or greater
+ *  than second string in terms of lexicographical order.
+ * */
 int unicode_lex_cmp(const void* str_a, const void* str_b);
+
+/** Compare two utf8-encoded strings ignoring all symbols except letters and case-insensitively
+ *  and return negative, zero or positive value if first string is less, equal or greater
+ *  than second string in terms of reversed lexicographical order.
+ * */
 int unicode_rev_lex_cmp(const void* str_a, const void* str_b);
